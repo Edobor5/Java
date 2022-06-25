4fc4fcdf4729a7b50592b87aa2388936e8b71584
Francisca <edoborfrancisca@gmail.com>
2022-06-25 09:46:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var opr = prompt("Operator: ")
 var num1 = parseInt(prompt("Number 1:"))
 var num2 = parseInt(prompt("Number 2: "))
 
-if(opr == "*")
+if(opr == "+")
 {
     alert(num1 + num2)
 } else if(opr == "-")
